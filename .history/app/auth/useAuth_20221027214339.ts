28ebf959294e133dce56aa9c579c500436b32e38
George <george.fons@gmail.com>
2022-10-27 22:14:02 -0300

    Login with Google, user picture and name

---
+++ .history/app/auth/useAuth_20221027214339.ts
@@ -0,0 +1,37 @@
+import { useContext } from "react"
+import jwtDecode from "jwt-decode"
+
+import AuthContext from "./context";
+import authStorage from "./storage";
+import { environment } from "../../enviroment";
+import users from "../api/users";
+import useApi from "../hooks/useApi";
+
+const apiParam = "?X-TRENA-KEY=" + environment.apiKey
+
+const endpoint = "/security/users/me" + apiParam;
+
+export default function useAuth() {
+  const {user, setUser, userData} = useContext(AuthContext);
+
+  const logIn = async (authToken: any) => {
+    const user = jwtDecode(authToken);
+    setUser(user);
+
+    // const { data: userData, request: getUserData } = useApi(
+    //   users.getUserData(authToken)
+    // );
+    // await getUserData();
+    
+    console.log("User:", user)
+    // console.log("UserData:", userData)
+    authStorage.storeToken(authToken);
+  }
+
+  const logOut = () => {
+    setUser(null);
+    authStorage.removeToken();
+  }
+
+  return {user, logIn, logOut, setUser, userData};
+}
